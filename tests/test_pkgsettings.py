5ea9e47cbbdc062ab5b05c9244027853930ae283
Niels Lensink <niels.lenssink@kpn.com>
2016-03-09 17:59:02 +0100

    OPT increased test coverage to 100% and flipped asserts to be expected, actual

--- tests/test_pkgsettings.py
+++ tests/test_pkgsettings.py
@@ -21,32 +21,36 @@ class TestPkgsettings(unittest.TestCase):
         super(TestPkgsettings, self).setUp()
 
     def test_default_settings(self):
-        self.assertEqual(settings.test_key, 'test_value')
+        self.assertEqual('test_value', settings.test_key)
 
     def test_context_manager(self):
         with settings(test_key='context_manager'):
-            self.assertEqual(settings.test_key, 'context_manager')
-        self.assertEqual(settings.test_key, 'test_value')
+            self.assertEqual('context_manager', settings.test_key)
+        self.assertEqual('test_value', settings.test_key)
 
     def test_decorator(self):
         @settings(test_key='decorator')
         def go():
-            self.assertEqual(settings.test_key, 'decorator')
+            self.assertEqual('decorator', settings.test_key)
 
         go()
-        self.assertEqual(settings.test_key, 'test_value')
+        self.assertEqual('test_value', settings.test_key)
 
     def test_decorator_in_class(self):
-        unittest_self = self
+        _self = self
 
         class Dummy(object):
             @settings(test_key='decorator_in_class')
             def go(self):
-                unittest_self.assertEqual(settings.test_key,
-                                          'decorator_in_class')
+                _self.assertEqual('decorator_in_class', settings.test_key)
 
         Dummy().go()
-        self.assertEqual(settings.test_key, 'test_value')
+        self.assertEqual('test_value', settings.test_key)
+
+    def test_as_dict(self):
+        with settings(test=True):
+            expected = dict(test=True, test_key='test_value')
+            self.assertEqual(expected, settings.as_dict())
 
 
 if __name__ == '__main__':
