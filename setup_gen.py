0c46f4de220a7bb0b010d076d06b0a0541af0926
Michiel Holtkamp <michiel@elements.nl>
2017-06-23 14:37:14 +0200

    FIX - Remove tests from package
    Having a 'tests' module installed will pollute the
    namespace. Tests should only be necessary before
    publishing.

--- setup_gen.py
+++ setup_gen.py
@@ -12,7 +12,7 @@ write_setup_py(
     author_email='niels.lenssink@kpn.com',
     url='https://github.com/kpn-digital/py-pkgsettings.git',
     install_requires=list_requirements('requirements/requirements-base.txt'),
-    packages=find_packages(exclude=['pkgsettings.tests*']),
+    packages=find_packages(exclude=['tests*']),
     tests_require=['tox'],
     include_package_data=True,
     zip_safe=False,
